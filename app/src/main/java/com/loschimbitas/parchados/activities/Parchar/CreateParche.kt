64f7ee47b4f0bd7e1768d5b2aefcf932ac556b9b
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-09-09 18:05:05 -0500

    create CreateParche activity

---
+++ app/src/main/java/com/loschimbitas/parchados/activities/Parchar/CreateParche.kt
@@ -0,0 +1,12 @@
+package com.loschimbitas.parchados.activities.Parchar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loschimbitas.parchados.R
+
+class CreateParche : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_parche)
+    }
+}
