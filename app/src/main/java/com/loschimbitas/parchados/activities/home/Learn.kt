b6e3f04988ca988de955b34f870b2a13f18f43f5
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-09-10 03:19:39 -0500

    added functions and comments

--- app/src/main/java/com/loschimbitas/parchados/activities/home/Learn.kt
+++ app/src/main/java/com/loschimbitas/parchados/activities/home/Learn.kt
@@ -3,6 +3,7 @@ package com.loschimbitas.parchados.activities.home
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.loschimbitas.parchados.activities.learning.CreateClass
 import com.loschimbitas.parchados.databinding.ActivityLearnBinding
 
 class Learn : AppCompatActivity() {
@@ -13,11 +14,6 @@ class Learn : AppCompatActivity() {
         binding = ActivityLearnBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonParchar.setOnClickListener {
-            startActivity(Intent(this, Parchar::class.java))
-            finish()
-        }
-
 //        For users that have not permission to start class (students)
 //        binding.linearLayoutStartClass.layoutParams.height = (5 * getResources().getDisplayMetrics().density + 0.5f).toInt()
 //        binding.textViewStartClass.text = ""
@@ -25,5 +21,56 @@ class Learn : AppCompatActivity() {
 //        binding.buttonStartClass.visibility = LinearLayout.INVISIBLE
 //        binding.buttonStartClass.isEnabled = false
 
+    }
+
+    /**
+     * @Name: initListeners
+     * @Description: Initialize the listeners for the buttons in the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initialize() {
+        initListeners()
+    }
+
+    /**
+     * @Name: initListeners
+     * @Description: Initialize the listeners for the buttons in the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initListeners() {
+        setUpCreateClassListener()
+        setUpParcharListener()
+    }
+
+    /**
+     * @Name: setUpCreateClassListener
+     * @Description: Set up the listener for the button that takes the user to the CreateClass activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun setUpCreateClassListener() {
+        binding.buttonStartClass.setOnClickListener {
+            startActivity(Intent(this, CreateClass::class.java))
+            finish()
+        }
+    }
+
+    /**
+     * @Name: setUpParcharListener
+     * @Description: Set up the listener for the button that takes the user to the Parchar activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun setUpParcharListener() {
+        binding.buttonParchar.setOnClickListener {
+            startActivity(Intent(this, Parchar::class.java))
+            finish()
+        }
     }
 }
