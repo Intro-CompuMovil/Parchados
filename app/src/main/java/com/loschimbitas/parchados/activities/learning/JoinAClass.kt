709d020c9833f8bd63193b81073f220e20ced9c4
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-09-10 16:32:14 -0500

    added functions to connect activities JoinAParche, JoinACLass -> InProgress

--- app/src/main/java/com/loschimbitas/parchados/activities/learning/JoinAClass.kt
+++ app/src/main/java/com/loschimbitas/parchados/activities/learning/JoinAClass.kt
@@ -1,7 +1,9 @@
 package com.loschimbitas.parchados.activities.learning
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.loschimbitas.parchados.activities.active.InProgress
 import com.loschimbitas.parchados.databinding.ActivityJoinAClassBinding
 
 class JoinAClass : AppCompatActivity() {
@@ -12,5 +14,45 @@ class JoinAClass : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityJoinAClassBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initialize()
+    }
+
+    /**
+     * @Name: initListeners
+     * @Description: Initialize the listeners for the buttons in the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initialize() {
+        initListeners()
+    }
+
+    /**
+     * @Name: initListeners
+     * @Description: Initialize the listeners for the buttons in the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initListeners() {
+        setUpButtonListener()
+    }
+
+    /**
+     * @Name: setUpButtonListener
+     * @Description: Set up the listener for the button that takes the user to the InProgress activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun setUpButtonListener() {
+        binding.button.setOnClickListener {
+            val intent = Intent(this, InProgress::class.java)
+            intent.putExtra("type", "class")
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/loschimbitas/parchados/activities/parchar/JoinAParche.kt
+++ app/src/main/java/com/loschimbitas/parchados/activities/parchar/JoinAParche.kt
@@ -1,7 +1,9 @@
 package com.loschimbitas.parchados.activities.parchar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.loschimbitas.parchados.activities.active.InProgress
 import com.loschimbitas.parchados.databinding.ActivityJoinAParcheBinding
 
 class JoinAParche : AppCompatActivity() {
@@ -12,5 +14,45 @@ class JoinAParche : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityJoinAParcheBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        initialize()
+    }
+
+    /**
+     * @Name: initListeners
+     * @Description: Initialize the listeners for the buttons in the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initialize() {
+        initListeners()
+    }
+
+    /**
+     * @Name: initListeners
+     * @Description: Initialize the listeners for the buttons in the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initListeners() {
+        setUpButtonListener()
+    }
+
+    /**
+     * @Name: setUpButtonListener
+     * @Description: Set up the listener for the button that takes the user to the InProgress activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun setUpButtonListener() {
+        binding.button.setOnClickListener {
+            val intent = Intent(this, InProgress::class.java)
+            intent.putExtra("type", "parche")
+            startActivity(intent)
+            finish()
+        }
     }
 }
