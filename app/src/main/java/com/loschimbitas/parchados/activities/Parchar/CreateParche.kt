a656a1fa5cc079ad53c5f3681dfb9b93f0575e68
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-09-09 21:08:45 -0500

    added functions to change activities

--- app/src/main/java/com/loschimbitas/parchados/activities/Parchar/CreateParche.kt
+++ app/src/main/java/com/loschimbitas/parchados/activities/Parchar/CreateParche.kt
@@ -1,12 +1,47 @@
 package com.loschimbitas.parchados.activities.Parchar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.loschimbitas.parchados.R
+import com.loschimbitas.parchados.activities.selectplacemap.SelectPlace
+import com.loschimbitas.parchados.databinding.ActivityCreateParcheBinding
 
 class CreateParche : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCreateParcheBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_create_parche)
+        binding = ActivityCreateParcheBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initListeners()
     }
+
+    /**
+     * @Name: initListeners
+     * @Description: Initialize the listeners for the buttons in the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initListeners() {
+        setUpSelectPlaceOnMap()
+    }
+
+    /**
+     * @Name: setUpSelectPlaceOnMap
+     * @Description: Set up the listener for the select place on map button.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun setUpSelectPlaceOnMap() {
+        binding.buttonSelectPlaceOnMap.setOnClickListener {
+            val intent = Intent(this, SelectPlace::class.java)
+            intent.putExtra("root", "CreateParche")
+            startActivity(intent)
+        }
+    }
+
+
 }
