7e9c0f5b019be38d95bef1253931ff5095944387
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-09-10 03:42:17 -0500

    added functions

--- app/src/main/java/com/loschimbitas/parchados/activities/learning/CreateClass.kt
+++ app/src/main/java/com/loschimbitas/parchados/activities/learning/CreateClass.kt
@@ -1,15 +1,36 @@
 package com.loschimbitas.parchados.activities.learning
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.loschimbitas.parchados.activities.selectplacemap.SelectPlace
 import com.loschimbitas.parchados.databinding.ActivityCreateClassBinding
 
 class CreateClass : AppCompatActivity() {
 
     private lateinit var binding: ActivityCreateClassBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCreateClassBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initialize()
+    }
+
+    private fun initialize() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        setUpCreateClassListener()
+    }
+
+    private fun setUpCreateClassListener() {
+        binding.buttonSelectPlaceOnMap.setOnClickListener {
+            val intent = Intent(this, SelectPlace::class.java)
+            intent.putExtra("root", "CreateClass")
+            startActivity(intent)
+            finish()
+        }
     }
 }
