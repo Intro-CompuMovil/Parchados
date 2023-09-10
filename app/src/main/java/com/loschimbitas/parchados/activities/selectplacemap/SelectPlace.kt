02e9aa37dc0b5dc10694107d0ad9c1e7d03e99d2
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-09-09 20:57:46 -0500

    create SelectPlace

---
+++ app/src/main/java/com/loschimbitas/parchados/activities/selectplacemap/SelectPlace.kt
@@ -0,0 +1,12 @@
+package com.loschimbitas.parchados.activities.selectplacemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.loschimbitas.parchados.R
+
+class SelectPlace : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_place)
+    }
+}
