c7d458da795e4bf2e068b554ef7b03a77326f483
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2023-09-11 17:49:11 -0500

    create configuration menu

---
+++ app/src/main/java/com/loschimbitas/parchados/activities/configuration/ConfigurationMenu.kt
@@ -0,0 +1,40 @@
+package com.loschimbitas.parchados.activities.configuration
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.loschimbitas.parchados.databinding.ActivityConfigurationMenuBinding
+
+class ConfigurationMenu : AppCompatActivity() {
+
+    private lateinit var binding: ActivityConfigurationMenuBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityConfigurationMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initialize()
+    }
+
+    /**
+     * @Name: initialize
+     * @Description: Initialize the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initialize() {
+        initListeners()
+    }
+
+    /**
+     * @Name: initListeners
+     * @Description: Initialize the listeners for the buttons in the activity.
+     * @Parameters: None.
+     * @Return: None.
+     * @Throws: None.
+     */
+    private fun initListeners() {
+
+    }
+}
