4e09ec12c6ca2a1495126c05abda38e2e6f9d351
davidfer1112 <davidfernando1112@gmail.com>
2023-10-17 08:06:38 -0500

    Feature: marcadores aprender

--- app/src/main/java/com/loschimbitas/parchados/activities/home/Learn.kt
+++ app/src/main/java/com/loschimbitas/parchados/activities/home/Learn.kt
@@ -11,12 +11,14 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.net.toUri
+import com.loschimbitas.parchados.R
 import com.loschimbitas.parchados.activities.configuration.ConfigurationMenu
 import com.loschimbitas.parchados.activities.globales.Globales
 import com.loschimbitas.parchados.activities.learning.CreateClass
 import com.loschimbitas.parchados.activities.learning.JoinAClass
 import com.loschimbitas.parchados.databinding.ActivityLearnBinding
 import com.loschimbitas.parchados.databinding.ActivityParcharBinding
+import org.osmdroid.api.IMapController
 import org.osmdroid.bonuspack.routing.OSRMRoadManager
 import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.config.Configuration
@@ -25,6 +27,7 @@ import org.osmdroid.library.BuildConfig
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.overlay.MapEventsOverlay
+import org.osmdroid.views.overlay.Marker
 import org.osmdroid.views.overlay.Polyline
 
 class Learn : AppCompatActivity() {
@@ -90,6 +93,25 @@ class Learn : AppCompatActivity() {
         // ubicacon actual
         val currentLocation = getCurrentLocation()
 
+        // Verificacion de ubicacion actual
+        if (currentLocation != null) {
+            val mapController: IMapController = binding.osmMap.controller
+
+            mapController.setZoom(18.0)
+            mapController.setCenter(GeoPoint(currentLocation.latitude, currentLocation.longitude))
+
+            // establecimiento de puntos(marcadores)
+            showMarker(GeoPoint(currentLocation.latitude, currentLocation.longitude),"Mi ubicacion","UBI")
+            showMarker(GeoPoint(4.6318,-74.0667),"Ping Pong", "PING")
+            showMarker(GeoPoint(4.6285,-74.0647),"Fulbol","FUTB")
+            showMarker(GeoPoint(4.6256,-74.0653),"Tenis","TENI")
+            showMarker(GeoPoint(4.6454,-74.0618),"Voleibol","VOLE")
+            showMarker(GeoPoint(4.6318,-74.0615),"Basketball","BASK")
+
+        } else {
+            Toast.makeText(this, "Ubicación no encontrada", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
     private fun getCurrentLocation(): Location? {
@@ -110,6 +132,33 @@ class Learn : AppCompatActivity() {
             e.printStackTrace()
             null
         }
+    }
+
+    private fun showMarker(geoPoint: GeoPoint, markerName: String, tipo: String) {
+        // Crea y muestra un nuevo marcador en la ubicación proporcionada
+        val marker = Marker(binding.osmMap)
+        marker.title = markerName
+        marker.position = geoPoint
+
+        marker.setOnMarkerClickListener { _, _ ->
+            calculateRouteToMarker(geoPoint)
+            showCustomToast(markerName, getMarkerIconResource(tipo))
+            true
+        }
+
+        marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+
+        // Icono personalizado según el deporte
+        when(tipo){
+            "UBI" -> marker.icon = resources.getDrawable(R.drawable.persona, theme)
+            "PING" -> marker.icon = resources.getDrawable(R.drawable.pingpong, theme)
+            "TENI" -> marker.icon = resources.getDrawable(R.drawable.tenis, theme)
+            "BASK" -> marker.icon = resources.getDrawable(R.drawable.basketball, theme)
+            "FUTB" -> marker.icon = resources.getDrawable(R.drawable.futbol, theme)
+            "VOLE" -> marker.icon = resources.getDrawable(R.drawable.voleibol, theme)
+        }
+
+        binding.osmMap.overlays.add(marker)
     }
 
     override fun onPause() {
